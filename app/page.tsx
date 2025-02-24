b04766760170593ab831e1ec81f154430cbd8668
yoarajota <joaovbscontato@gmail.com>
2025-02-24 20:53:35 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-"use client";
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
-import Background from "@/components/Background";
-
-// Uso do componente Grid com espaçamento configurável
-export const App = () => <Background></Background>;
-
-export default App;
+export default async function Home() {
+  return (
+    <>
+      <Hero />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
+    </>
+  );
+}
--- components/Background.tsx
+++
@@ -1,6 +0,0 @@
-export default function Background() {
-  return (
-    <body>
-    </body>
-  );
-}

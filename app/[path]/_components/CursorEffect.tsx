94db9b56f7255d41a990f1633f62fc84b5d81b3b
yoarajota <joaovbscontato@gmail.com>
2025-03-25 22:47:01 -0300

    feat: partial feature svg

--- app/[path]/_components/CursorEffect.tsx
+++ app/[path]/_components/CursorEffect.tsx
@@ -8,10 +8,43 @@ const transition = {
   ease: [0, 0.71, 0.2, 1.01],
 };
 
+
+// #FF0066;
+// #BB004B;
+
+const SVGFull = () => {
+  return (
+    <g
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M87.5 -21.8C101.4 14.4 92.4 64.6 64.8 83.3C37.3 101.9 -8.9 88.9 -46.6 62C-84.3 35.1 -113.5 -5.6 -104.3 -35.3C-95.1 -64.9 -47.6 -83.5 -5.4 -81.7C36.8 -79.9 73.5 -57.9 87.5 -21.8"
+        fill="#BB004B"
+      />
+    </g>
+  );
+}
+
+const SVGOutlined = () => {
+  return (
+    <g
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M87.5 -21.8C101.4 14.4 92.4 64.6 64.8 83.3C37.3 101.9 -8.9 88.9 -46.6 62C-84.3 35.1 -113.5 -5.6 -104.3 -35.3C-95.1 -64.9 -47.6 -83.5 -5.4 -81.7C36.8 -79.9 73.5 -57.9 87.5 -21.8"
+        fill="none"
+        stroke="#BB004B"
+        strokeWidth="6"
+      />
+    </g>
+  );
+}
+
 export default function CursorEffect() {
   const cursorRef = useRef(null);
   const controls = useAnimation();
   const [hasMoved, setHasMoved] = useState(false);
+  const [isHovering, setIsHovering] = useState(false); // New state for hover
 
   useEffect(() => {
     const cursor = cursorRef.current;
@@ -23,8 +56,8 @@ export default function CursorEffect() {
           left: e.clientX - 15,
           top: e.clientY - 10,
         });
+
         controls.start({
-          opacity: 1,
           scale: 1,
           transition,
         });
@@ -45,23 +78,17 @@ export default function CursorEffect() {
         element.tagName === "A" ||
         element.tagName === "BUTTON"
       );
-    }
+    };
 
     const onMouseOver = (e: MouseEvent) => {
       if (isPressable(e.target as HTMLElement)) {
-        controls.start({
-          opacity: 0.5,
-          transition,
-        });
+        setIsHovering(true); 
       }
     };
 
     const onMouseOut = (e: MouseEvent) => {
       if (isPressable(e.target as HTMLElement)) {
-        controls.start({
-          opacity: 1,
-          transition,
-        });
+        setIsHovering(false);
       }
     };
 
@@ -80,13 +107,20 @@ export default function CursorEffect() {
     <motion.div
       ref={cursorRef}
       className="w-8 h-8 rounded-full pointer-events-none z-50 mix-blend-difference cursor-none"
-      initial={{ opacity: 0, scale: 0.5, left: 0, top: 0 }}
+      initial={{ left: 0, top: 0 }}
       animate={controls}
       style={{
-        background: "rgba(255, 255, 255, 0.5)",
-        backdropFilter: "blur(4px)",
         position: "absolute",
       }}
-    />
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        viewBox="0 0 24 24"
+      >
+        {isHovering ? <SVGOutlined /> : <SVGFull />}
+      </svg>
+    </motion.div>
   );
 }

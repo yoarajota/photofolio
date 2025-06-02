bbaad73aed45e2df57a08bb38fac3e0c631c347b
yoarajota <joaovbscontato@gmail.com>
2025-06-02 00:02:44 -0300

    feat: return button

--- app/protected/jobs/[id]/page.tsx
+++ app/protected/jobs/[id]/page.tsx
@@ -8,10 +8,11 @@ import {
 } from "@/components/ui/card";
 import { getJob } from "../actions";
 import { Job } from "@/types";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
-export default async function Web(props: {
-  params: Promise<{ id: number }>;
-}) {
+export default async function Web(props: { params: Promise<{ id: number }> }) {
   const params: {
     id: number;
   } = await props.params;
@@ -21,15 +22,21 @@ export default async function Web(props: {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Altere a página de um trabalho</CardTitle>
+        <CardTitle>
+          <Link href="/protected/jobs">
+            <Button size="icon" variant="outline" className="mr-2">
+              <ChevronLeft className="h-4 w-4" />
+            </Button>
+          </Link>
+          
+          Altere a página de um trabalho
+        </CardTitle>
         <CardDescription>
           Configure as páginas dos trabalhos que você fez.
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <JobsForm 
-          data={job.data as Job}
-        />
+        <JobsForm data={job.data as Job} />
       </CardContent>
     </Card>
   );
